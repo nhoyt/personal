a00212212b9f28e3541d13ba14a7b15f3ad05b9a
Nicholas Hoyt <nick@webui.com>
2022-02-10 09:11:30 -0600

    Rename resume files

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 SRC="$HOME/Documents/Office"
 DEST="./resume"
 
-cp "$SRC/nicholas-hoyt-fed.pdf" "$DEST/"
-cp "$SRC/nicholas-hoyt-uxd.pdf" "$DEST/"
+cp "$SRC/Resume-FED.pdf" "$DEST/nhoyt-fed.pdf"
+cp "$SRC/Resume-UXD.pdf" "$DEST/nhoyt-uxd.pdf"
