a97817cf614c9a55479edec9faefbc05a423f361
Nicholas Hoyt <nick@webui.com>
2022-05-02 08:06:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SRC="$HOME/Documents/Office"
+SRC="$HOME/Documents/Office/Resume"
 DEST="./resume"
 
-cp "$SRC/Resume-FED.pdf" "$DEST/nhoyt-fed.pdf"
-cp "$SRC/Resume-UXD.pdf" "$DEST/nhoyt-uxd.pdf"
+cp "$SRC/FED.pdf" "$DEST/nhoyt-fed.pdf"
+cp "$SRC/UXD.pdf" "$DEST/nhoyt-uxd.pdf"
