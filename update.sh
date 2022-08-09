a481287cf70fd4b705321a36da0a990ac824c8c0
Nicholas Hoyt <nick@webui.com>
2022-08-09 09:01:49 -0500

    Content updates

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@
 SRC="$HOME/Documents/Office/Resume"
 DEST="./resume"
 
-cp "$SRC/FED.pdf" "$DEST/nhoyt-fed.pdf"
-cp "$SRC/UXD.pdf" "$DEST/nhoyt-uxd.pdf"
+# cp "$SRC/FED.pdf" "$DEST/nhoyt-fed.pdf"
+# cp "$SRC/UXD.pdf" "$DEST/nhoyt-uxd.pdf"
+
+cp "$SRC/Resume-1PA.pdf" "$DEST/nhoyt-resume.pdf"
